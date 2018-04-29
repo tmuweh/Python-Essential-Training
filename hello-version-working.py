4d5c3b6f45ffda15ebd829240ad6dcc8f23f4c6c
Tangue Muweh <atmuweh@gmail.com>
2018-04-29 22:24:23 +0200

    Display python version

---
+++ hello-version-working.py
@@ -0,0 +1,12 @@
+
+#tmuweh
+
+import platform
+
+def main():
+    message()
+
+def message():
+    print('This is python version {}'.format(platform.python_version()))
+
+if __name__ == '__main__': main()
