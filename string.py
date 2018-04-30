61ad7e03e3c8071d9065d7709fcc3572a44b9e44
Tangue Muweh <atmuweh@gmail.com>
2018-04-30 02:10:45 +0200

    string object basic function

---
+++ string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+x = 'seven'.upper()
+y = 'seven'.capitalize()
+z = 'seven'.lower()
+print('{}{}{}'.format(x, y, z))
+print(type(x))
+
+#f string
+
+a = 8
+b = 9
+x = f'seven {a} {b}'
+print("{}". format(x))
