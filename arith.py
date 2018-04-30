32a1444c98e8022214272312a264fb00633ce950
Tangue Muweh <atmuweh@gmail.com>
2018-04-30 12:33:33 +0200

    add subt multi div posi nega

---
+++ arith.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+x = 5
+y = 3
+
+z = x + y
+
+print(f'result is {z}')
+
+z = x - y
+print(f'result is {z}')
+
+z = x / y
+print(f'result is {z}')
+
+z = x // y
+print(f'result is {z}')
+
+z = x * y
+print(f'result is {z}')
+
+z = +z
+print(f'result is {z}')
+
+z = -z
+print(f'result is {z}')
+
