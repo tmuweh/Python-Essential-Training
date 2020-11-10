44f7c41e9de9c50e92d3fd6f696432a4c27aec10
tmuweh <atmuweh@gmail.com>
2020-11-10 14:34:34 +0100

    Create dice.py
    row dice 3 times

---
+++ dice.py
@@ -0,0 +1,15 @@
+
+# roll the dice 3 times and print the result
+
+import random
+
+def dice():
+    num = random.randint(1, 6)
+    print(num)
+
+print("First Roll")
+dice()
+print("Second Roll")
+dice()
+print("Third Roll")
+dice()
